1b72deab7f90a212aa1e23182039c14ddad76504
HeyTymon <tymon.email.praca@gmail.com>
2025-04-07 13:31:42 +0200

    gitignore added

--- Basics/conditionsBasics.sh
+++ Basics/conditionsBasics.sh
@@ -6,11 +6,21 @@
 var1=5
 var2=10
 
-[[ "${var1}" == 5 ]]
-echo "Exit code: ${?}" #true
+[[ "${var1}" == 5 ]] ; echo "Exit code: ${?}" #true
 
-[[ "${var1}" == "$(( $var2 - 5))" ]]
-echo "Exit code: ${?}" #true
+[[ "${var1}" == "$(( var2 - 5))" ]] ; echo "Exit code: ${?}" #true
 
-[[ "$(echo "10/2" | bc)" == "$(( $var2 - $var1 ))" ]]
-echo "Exit code: ${?}" #true
+[[ "$(echo "10/2" | bc)" == "$(( var2 - var1 ))" ]] ; echo "Exit code: ${?}" #true
+
+[[ "$(echo '1 2 3 4 5' | wc -w)" == 5 ]] ; echo "Exit code: ${?}" #true
+
+[[ "Tymon" == T[yi]m* ]] ; echo "Exit code: ${?}" #true
+
+[[ "${var1}" -lt "$(( var2 - 4 ))" ]] ; echo "Exit code: ${?}" #true
+
+[[ 0 -le "$(( 1 < 2 ))" ]] ; echo "Exit code: ${?}" #true
+
+[[ "1" < "2" ]] && [[ "10" < "2" ]] ; echo "Exit code: ${?}" #true -> alphabetically sorted values (1 is before 2)
+
+[[ "${var1}" -gt 3 && "${var2}" -lt 12 ]] ; echo "Exit code: ${?}" #true
+
---
+++ Basics/romeoNjuliet.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [[ ! -e "./files/romeo.txt" ]] ; then
+	wget -P ./files 'https://downloads.codingcoursestv.eu/055%20-%20bash/if/romeo.txt'
+fi
+
+
+
